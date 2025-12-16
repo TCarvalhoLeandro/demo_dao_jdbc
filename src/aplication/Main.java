f5565e6e8ff129dd37f70636ee47a1f2aaff990d
Leandro <trairicarvalho@gmail.com>
2025-12-16 19:38:21 -0300

    Criacao da classe Seller

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,6 +1,9 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
@@ -8,6 +11,9 @@ public class Main {
 		
 		Department dep = new Department(1,"Books");
 		System.out.println(dep);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 2000.0, dep);
+		System.out.println(seller);
 
 	}
 
